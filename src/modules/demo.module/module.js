6d24de09b84e9219fe8d8c912ff4d22e7cc045c9
David <2527460+david-mooney@users.noreply.github.com>
2023-02-07 01:14:03 +0000

    added sample project files

---
+++ src/modules/demo.module/module.js
@@ -0,0 +1,6 @@
+(() => {
+  window.addEventListener('DOMContentLoaded', () => {
+    // eslint-disable-next-line no-console
+    console.log('Demo module loaded');
+  });
+})();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { cmsConfig } = require('@hs-web-team/webpack-config');
+const { merge } = require('webpack-merge');
+const path = require('path');
+
+const modes = {
+  DEVELOPMENT: 'development',
+  PRODUCTION: 'production',
+};
+
+const customConfig = {
+  // Add your custom config settings here
+};
+
+module.exports = (_, args) => {
+  if (args.mode === modes.DEVELOPMENT) {
+    const devConfig = cmsConfig({
+      projectFolder: path.resolve(__dirname),
+      portal: 'DEV',
+      cmsSrc: 'dist',
+      cmsDest: 'sample-site',
+      autoupload: true,
+    });
+
+    return merge(devConfig, customConfig);
+  }
+
+  if (args.mode === modes.PRODUCTION) {
+    const prodConfig = cmsConfig({
+      projectFolder: path.resolve(__dirname),
+      portal: 'PROD',
+      cmsSrc: 'dist',
+      cmsDest: 'sample-site',
+    });
+
+    return merge(prodConfig, customConfig);
+  }
+
+  throw new Error('Error: no webpack mode specified.');
+}
